dbc0e7f6814f3d816d4299929fa1a39c0ee12b01
kamal <medkamalberrami@gmail.com>
2019-03-25 12:21:24 +0100

    mise en place d une template thymeleafe for hello world

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,39 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DemoController {
+	
+	// inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "helloworld"; //view
+    }
+
+    // /hello?name=kotlin
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "") 
+			String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "helloworld"; //view
+    }
+}
